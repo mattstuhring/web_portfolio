db78b1cce483b14f94abdf5b79157b0d76ff1014
Matt Stuhring <whereyouatmatt@gmail.com>
2017-02-13 09:50:21 -0800

    bootstrap modals complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+$('#matchday').modal(options);
+
+$('#stayup').modal(options);
+
+$('#concert').modal(options);
+
+$('#bitesize').modal(options);
+
+$('#hidef').modal(options);
+
+$('#coffee').modal(options);
